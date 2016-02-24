343bcd8b210a2e70bb59477e1413ff144e87bf18
Sebastian Monte <sebastian.h.monte@gmail.com>
2016-02-24 12:38:08 +0100

    Shorter CSS selectors
    If element has a name attribute, check the uniqueness of that and
    use it as the only selector if it is unique.
    
    Also fixed tests to use a different document (it was using Jasmine's HTML
    DOM which was wrong).
    
    Fixes issue #4

--- code/spec/XPathGeneratorSpec.js
+++ code/spec/XPathGeneratorSpec.js
@@ -1,64 +1,68 @@
-describe('XPathGenerator', function() {
+describe('XPathGenerator', function () {
 
-	describe('DOM with IDs', function() {
+    describe('DOM with IDs', function () {
+        var createdDom;
 
-		it("finds element with id", function() {
-			targetElement = createDocWithTargetElementThatHasId('<div id="target-element"></div>');
-			expect(XPathGenerator(targetElement)).toEqual('//*[@id=\'target-element\']');
-		});
-	});
+        it("finds element with id", function () {
+            createdDom = createDocWithTargetElementThatHasId('<div id="target-element"></div>');
+            expect(XPathGenerator(createdDom.targetElement, createdDom.doc)).toEqual('//*[@id=\'target-element\']');
+        });
+    });
 
-	describe('DOM with classses', function() {
-		var targetElement;
+    describe('DOM with classses', function () {
+        var createdDom;
 
-		it("finds element with class that has siblings of same type", function() {
-			targetElement = createDocWithTargetElement('<div id="target-element" class="element-class"></div><div class="another-element-class"></div>');
-			expect(XPathGenerator(targetElement)).toEqual('/html/body/div[1]');
-		});
+        it("finds element with class that has siblings of same type", function () {
+            createdDom = createDocWithTargetElement('<div id="target-element" class="element-class"></div><div class="another-element-class"></div>');
+            expect(XPathGenerator(createdDom.targetElement, createdDom.doc)).toEqual('/html/body/div[1]');
+        });
 
-		it("finds element with class that has no siblings of same type", function() {
-			targetElement = createDocWithTargetElement('<div id="target-element" class="element-class"></div><li class="another-element-class"></li>');
-			expect(XPathGenerator(targetElement)).toEqual('/html/body/div');
-		});
+        it("finds element with class that has no siblings of same type", function () {
+            createdDom = createDocWithTargetElement('<div id="target-element" class="element-class"></div><li class="another-element-class"></li>');
+            expect(XPathGenerator(createdDom.targetElement, createdDom.doc)).toEqual('/html/body/div');
+        });
 
-		it("finds first element with class that siblings also have", function() {
-			targetElement = createDocWithTargetElement('<div id="target-element" class="element-class"></div><div class="element-class"></div>');
-			expect(XPathGenerator(targetElement)).toEqual('/html/body/div[1]');
-		});
+        it("finds first element with class that siblings also have", function () {
+            createdDom = createDocWithTargetElement('<div id="target-element" class="element-class"></div><div class="element-class"></div>');
+            expect(XPathGenerator(createdDom.targetElement, createdDom.doc)).toEqual('/html/body/div[1]');
+        });
 
-		it("finds second element with class that siblings also have", function() {
-			targetElement = createDocWithTargetElement('<div class="element-class"></div><div id="target-element" class="element-class"></div>');
-			expect(XPathGenerator(targetElement)).toEqual('/html/body/div[2]');
-		});
-	});
+        it("finds second element with class that siblings also have", function () {
+            createdDom = createDocWithTargetElement('<div class="element-class"></div><div id="target-element" class="element-class"></div>');
+            expect(XPathGenerator(createdDom.targetElement, createdDom.doc)).toEqual('/html/body/div[2]');
+        });
+    });
 
-	describe('DOM with inputs', function() {
+    describe('DOM with inputs', function () {
+        var createdDom;
 
-		it("finds element with input that has unique name", function() {
-			targetElement = createDocWithTargetElement('<input id="target-element" name="input-name" />');
-			expect(XPathGenerator(targetElement)).toEqual('/html/body/input');
-		});
+        it("finds element with input that has unique name", function () {
+            createdDom = createDocWithTargetElement('<input id="target-element" name="input-name" />');
+            expect(XPathGenerator(createdDom.targetElement, createdDom.doc)).toEqual('/html/body/input');
+        });
 
-		it("finds element with input that has nonunique name", function() {
-			targetElement = createDocWithTargetElement('<input name="input-name" /><input id="target-element" name="input-name" />');
-			expect(XPathGenerator(targetElement)).toEqual('/html/body/input[2]');
-		});
-	});
+        it("finds element with input that has nonunique name", function () {
+            createdDom = createDocWithTargetElement('<input name="input-name" /><input id="target-element" name="input-name" />');
+            expect(XPathGenerator(createdDom.targetElement, createdDom.doc)).toEqual('/html/body/input[2]');
+        });
+    });
 
-	describe('DOM with siblings of same type', function() {
+    describe('DOM with siblings of same type', function () {
+        var createdDom;
 
-		it("finds element that has siblings of same type", function() {
-			targetElement = createDocWithTargetElement('<div id="target-element"></div><div></div><div></div>');
-			expect(XPathGenerator(targetElement)).toEqual('/html/body/div[1]');
-		});
-	});
+        it("finds element that has siblings of same type", function () {
+            createdDom = createDocWithTargetElement('<div id="target-element"></div><div></div><div></div>');
+            expect(XPathGenerator(createdDom.targetElement, createdDom.doc)).toEqual('/html/body/div[1]');
+        });
+    });
 
-	describe('DOM with no siblings', function() {
+    describe('DOM with no siblings', function () {
+        var createdDom;
 
-		it("finds element that has no siblings", function() {
-			targetElement = createDocWithTargetElement('<div id="target-element"></div>');
-			expect(XPathGenerator(targetElement)).toEqual('/html/body/div');
-		});
-	});
+        it("finds element that has no siblings", function () {
+            createdDom = createDocWithTargetElement('<div id="target-element"></div>');
+            expect(XPathGenerator(createdDom.targetElement, createdDom.doc)).toEqual('/html/body/div');
+        });
+    });
 
 });
--- code/spec/fixture/fixture.js
+++ code/spec/fixture/fixture.js
@@ -1,19 +1,19 @@
 function createDocWithTargetElementThatHasId(html) {
-	var doc, targetElement;
+    var doc;
 
-	doc = document.implementation.createHTMLDocument('Document');
-	doc.body.innerHTML = html;
+    doc = document.implementation.createHTMLDocument('Document');
+    doc.body.innerHTML = html;
 
-	return doc.getElementById('target-element');
+    return {targetElement: doc.getElementById('target-element'), doc: doc};
 }
 
 function createDocWithTargetElement(html) {
-	var doc, targetElement;
-	
-	doc = document.implementation.createHTMLDocument('Document');
-	doc.body.innerHTML = html;
-	targetElement = doc.getElementById('target-element');
-	targetElement.removeAttribute('id');
-	
-	return targetElement;
+    var doc, targetElement;
+
+    doc = document.implementation.createHTMLDocument('Document');
+    doc.body.innerHTML = html;
+    targetElement = doc.getElementById('target-element');
+    targetElement.removeAttribute('id');
+
+    return {targetElement: targetElement, doc: doc};
 }
