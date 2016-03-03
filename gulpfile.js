824038d22b9012cedc0114246c3db1842ad1dfbd
Sebastian Monte <sebastian.h.monte@gmail.com>
2016-03-03 14:10:51 +0100

    Added Travis integration with gulp tests
    - Needed to remove String.startsWith usage, because PhantomJS browser
    does not support it.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
-var jshint = require('gulp-jshint');
-var gulp   = require('gulp');
+const gulp = require('gulp');
+const jshint = require('gulp-jshint');
+const jasmineBrowser = require('gulp-jasmine-browser');
 
-gulp.task('lint', function() {
+gulp.task('lint', function () {
     return gulp.src(['./code/js/*.js', './code/js/tools/*.js'])
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
+});
+
+gulp.task('test', function () {
+    return gulp.src(['./code/js/tools/*.js', './code/spec/fixture/*.js', './code/spec/*Spec.js'])
+        .pipe(jasmineBrowser.specRunner({console: true}))
+        .pipe(jasmineBrowser.headless());
 });
