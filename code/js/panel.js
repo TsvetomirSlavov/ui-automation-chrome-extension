35cd329a64678f0f7869dec90fa5319516bb32a2
Sebastian Monte <sebastian.h.monte@gmail.com>
2016-03-21 08:31:47 +0100

    Support iframes

--- code/js/panel.js
+++ code/js/panel.js
@@ -7,7 +7,7 @@ var uiAutomationApp = angular.module('uiAutomationApp', [
     'ngclipboard'
 ]);
 
-uiAutomationApp.controller('UIAutomationController', function ($scope) {
+uiAutomationApp.controller('UIAutomationController', function ($scope, $q) {
 
     $scope.xPathExpression = '';
     $scope.cssExpression = '';
@@ -18,15 +18,65 @@ uiAutomationApp.controller('UIAutomationController', function ($scope) {
         updateCssAndXpathExpressions();
     });
 
-    function updateCssAndXpathExpressions() {
-        chrome.devtools.inspectedWindow.eval('(' + XPathGenerator.toString() + ')($0)', function (res) {
-            $scope.xPathExpression = res;
-            $scope.$apply();
+    function isFrameActive(frameURL) {
+        var deferred = $q.defer();
+        chrome.devtools.inspectedWindow.eval('$0', {frameURL: frameURL}, function (res) {
+            if (res) {
+                deferred.resolve(frameURL);
+            } else {
+                deferred.reject();
+            }
         });
+        return deferred.promise;
+    }
 
-        chrome.devtools.inspectedWindow.eval('(' + CSSGenerator.toString() + ')($0)', function (res) {
-            $scope.cssExpression = res;
-            $scope.$apply();
+    function getFrameURL() {
+
+        function getFrameSources() {
+            var frames = document.getElementsByTagName('iframe');
+            var sources = [];
+            for (var i = 0; i < frames.length; i++) {
+                sources.push(frames[i].src);
+            }
+            return sources;
+        }
+
+        var deferred = $q.defer();
+
+        chrome.devtools.inspectedWindow.eval('(' + getFrameSources.toString() + ')()', function (frameURLs) {
+            for (var i = 0; i < frameURLs.length; i++) {
+                isFrameActive(frameURLs[i]).then(function (val) {
+                    deferred.resolve(val);
+                })
+            }
+        });
+        return deferred.promise;
+    }
+
+    function updateCssAndXpathExpressions() {
+
+        function updateWithOptions(options) {
+            chrome.devtools.inspectedWindow.eval('(' + XPathGenerator.toString() + ')($0)', options, function (res) {
+                $scope.xPathExpression = res;
+                $scope.$apply();
+            });
+
+            chrome.devtools.inspectedWindow.eval('(' + CSSGenerator.toString() + ')($0)', options, function (res) {
+                $scope.cssExpression = res;
+                $scope.$apply();
+            });
+        }
+
+        chrome.devtools.inspectedWindow.eval('$0', function (res) {
+            var options = {};
+            if (!res) {
+                getFrameURL().then(function (frameURL) {
+                    options.frameURL = frameURL;
+                    updateWithOptions(options);
+                });
+            } else {
+                updateWithOptions(options);
+            }
         });
     }
 
